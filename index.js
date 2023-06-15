7d977ee93648001ef112725760cce82f560f32cf
Pedro Lopes <lopescostapedrohenrique@gmail.com>
2023-06-15 11:15:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,83 +5,79 @@ const rastrearEncomendas = require('correios-brasil').rastrearEncomendas;
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => { res.send('API dos Correios'); });
-
 app.post('/rastrear', (req, res) => {
-  const codRastreio = req.body.codigos || [];
+    const codRastreio = req.body.codigos || [];
+    
+    if (codRastreio.length === 0) {
+        return res.status(400).json({ error: 'Nenhum código de rastreamento fornecido.' });
+    }
 
-  // body example with codes 'NL609847554BR', 'NL587173487BR': { "codigos": ["NL609847554BR", "NL587173487BR"] }
+    rastrearEncomendas(codRastreio)
+        .then((response) => {
+        const response_js = [];
 
-  if (codRastreio.length === 0) {
-    return res.status(400).json({ error: 'Nenhum código de rastreamento fornecido.' });
-  }
+        for (let i = 0; i < response.length; i++) {
+            const objeto = response[i];
+            const eventos = objeto.eventos.reverse();
+            const eventos_js = [];
 
-  rastrearEncomendas(codRastreio)
-    .then((response) => {
-      const response_js = [];
+            for (let j = 0; j < eventos.length; j++) {
+            const evento = eventos[j];
+            const data = new Date(evento.dtHrCriado);
+            let origem, destino;
 
-      for (let i = 0; i < response.length; i++) {
-        const objeto = response[i];
-        const eventos = objeto.eventos.reverse();
-        const eventos_js = [];
+            if (evento.descricao != 'Objeto em trânsito - por favor aguarde') {
+                destino = null;
 
-        for (let j = 0; j < eventos.length; j++) {
-          const evento = eventos[j];
-          const data = new Date(evento.dtHrCriado);
-          let origem, destino;
-
-          if (evento.descricao != 'Objeto em trânsito - por favor aguarde') {
-            destino = null;
-
-            if (Object.keys(evento.unidade.endereco).length === 0) {
-              if (evento.unidade.tipo === 'País') origem = evento.unidade.nome + ' - Internacional';
-              else origem = evento.unidade.nome;
+                if (Object.keys(evento.unidade.endereco).length === 0) {
+                if (evento.unidade.tipo === 'País') origem = evento.unidade.nome + ' - Internacional';
+                else origem = evento.unidade.nome;
+                } else {
+                    if (evento.unidade.tipo === 'Unidade de Logística Integrada')
+                        origem = 'Unidade de Logística - ' + evento.unidade.endereco.cidade + '/' + evento.unidade.endereco.uf;
+                    else origem = evento.unidade.tipo + ' - ' + evento.unidade.endereco.cidade + '/' + evento.unidade.endereco.uf;
+                }
             } else {
-              if (evento.unidade.tipo === 'Unidade de Logística Integrada')
+                if (evento.unidade.tipo === 'Unidade de Logística Integrada')
                 origem = 'Unidade de Logística - ' + evento.unidade.endereco.cidade + '/' + evento.unidade.endereco.uf;
-              else origem = evento.unidade.tipo + ' - ' + evento.unidade.endereco.cidade + '/' + evento.unidade.endereco.uf;
+                else origem = evento.unidade.tipo + ' - ' + evento.unidade.endereco.cidade + '/' + evento.unidade.endereco.uf;
+
+                if (evento.unidadeDestino.tipo === 'Unidade de Logística Integrada')
+                destino =
+                    'Unidade de Logística - ' +
+                    evento.unidadeDestino.endereco.cidade +
+                    '/' +
+                    evento.unidadeDestino.endereco.uf;
+                else
+                destino =
+                    evento.unidadeDestino.nome + ' - ' + evento.unidadeDestino.endereco.cidade + '/' + evento.unidadeDestino.endereco.uf;
             }
-          } else {
-            if (evento.unidade.tipo === 'Unidade de Logística Integrada')
-              origem = 'Unidade de Logística - ' + evento.unidade.endereco.cidade + '/' + evento.unidade.endereco.uf;
-            else origem = evento.unidade.tipo + ' - ' + evento.unidade.endereco.cidade + '/' + evento.unidade.endereco.uf;
 
-            if (evento.unidadeDestino.tipo === 'Unidade de Logística Integrada')
-              destino =
-                'Unidade de Logística - ' +
-                evento.unidadeDestino.endereco.cidade +
-                '/' +
-                evento.unidadeDestino.endereco.uf;
-            else
-              destino =
-                evento.unidadeDestino.nome + ' - ' + evento.unidadeDestino.endereco.cidade + '/' + evento.unidadeDestino.endereco.uf;
-          }
+            const evento_js = {
+                data: data.toISOString(),
+                descricao: evento.descricao,
+                origem: origem,
+                destino: destino,
+            };
 
-          const evento_js = {
-            data: data.toISOString(),
-            descricao: evento.descricao,
-            origem: origem,
-            destino: destino,
-          };
+            eventos_js.push(evento_js);
+            }
 
-          eventos_js.push(evento_js);
+            const objeto_js = {
+            objeto: objeto.codObjeto,
+            eventos: eventos_js,
+            };
+
+            response_js.push(objeto_js);
         }
 
-        const objeto_js = {
-          objeto: objeto.codObjeto,
-          eventos: eventos_js,
-        };
-
-        response_js.push(objeto_js);
-      }
-
-      res.json(response_js);
-    })
-    .catch((error) => {
-      res.status(500).json({ error: 'Erro ao rastrear encomendas.' });
+        res.json(response_js);
+        })
+        .catch((error) => {
+        res.status(500).json({ error: 'Erro ao rastrear encomendas.' });
+        });
     });
-});
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Servidor iniciado na porta 3000.');
+    app.listen(process.env.PORT || 3000, () => {
+    console.log('Servidor iniciado na porta 3000.');
 });
